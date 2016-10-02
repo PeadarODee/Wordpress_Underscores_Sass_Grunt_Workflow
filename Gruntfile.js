9db17cbb726b2027f4b60544a01bdc4c8a10739e
Peter <email>
2016-10-02 23:47:48 +0100

    adjusted Gruntfile.js to send processed css files to root rather than compiled directory. Also renamed original style.css for safety. Set up minified css as main theme stylesheet using ! at the end of comment to keep comment in minified code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,40 @@
-module.exports = function(grunt) {
-    
-    
+module.exports = function (grunt) {
     grunt.initConfig({
         
-        pkg: grunt.file.readJSON('package.json'), //locate the package.json file, read it.
+        pkg: grunt.file.readJSON('package.json'), 
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                    sourcemap: 'none'
+                }, 
+                
+                files: {
+                    'style-human.css' : 'sass/style.scss'
+                } 
+            }, 
+            
+            dist: {
+            options: {
+                style: 'compressed',
+                sourcemap: 'none'
+                },
+            
+            files: {
+                'style.css' : 'sass/style.scss'
+                }
+            } 
+        }, 
+        
         watch: {
             css: {
-                files: '**/*.scss', // any .scss file
-                tasks: ['sass'] // run the sass task
-            } //css task
-        } //watch for changes
-    }); //Configuration settings for Grunt
+                files: '**/*.scss', 
+                tasks: ['sass'] 
+            } 
+        } 
+    }); 
     
-    
-    grunt.loadNPMTasks('grunt-contrib-watch'); // Load the watch task runner
-    grunt.registerTask('default', ['watch']); //set default task to "watch" task
-    
-} //Main Function
+    grunt.loadNpmTasks('grunt-contrib-sass'); 
+    grunt.loadNpmTasks('grunt-contrib-watch'); 
+    grunt.registerTask('default', ['watch']); 
+} 
